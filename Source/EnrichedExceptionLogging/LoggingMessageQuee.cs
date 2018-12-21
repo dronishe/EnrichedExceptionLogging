aba6a424a526be1b2a83fc4f0c72bd2207759428
dronishe <dronishe@zmail.ru>
2018-12-21 12:51:52 +0300

    1. Solution create.
    2. LoggingMessage introduced

---
+++ Source/EnrichedExceptionLogging/LoggingMessageQuee.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace EnrichedExceptionLogging
+{
+    public class LoggingMessage<TState>
+    {
+        public LogLevel LogLevel;
+
+        public EventId EventId;
+
+        public string Message;
+
+
+        //EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter
+        public TState State { get; set; }
+    }
+
+    public interface ILoggingMessageQuee<TState>
+    {
+        void Enqueue(LoggingMessage<TState> item);
+        LoggingMessage<TState> Dequeue();
+        int Count { get; }
+        void Clear();
+    }
+
+    public class LoggingMessageQuee<TState> : Queue<LoggingMessage<TState>>, ILoggingMessageQuee<TState>
+    { }
+}
